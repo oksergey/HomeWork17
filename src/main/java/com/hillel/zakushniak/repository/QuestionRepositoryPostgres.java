022adc112e60307becd31b39c038a8928650fcd2
Zakushniak Sergii <ok.sergey@gmail.com>
2023-05-30 16:31:20 +0300

    questionarium /CRUD

---
+++ src/main/java/com/hillel/zakushniak/repository/QuestionRepositoryPostgres.java
@@ -0,0 +1,132 @@
+package com.hillel.zakushniak.repository;
+
+import com.hillel.zakushniak.model.Question;
+import com.hillel.zakushniak.repository.dao.QuestionRepository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionRepositoryPostgres implements QuestionRepository {
+
+
+    private final Connection connection;
+    public static final String save =
+            """
+                    INSERT INTO public.questions(name) 
+                    VALUES (?)
+                    """;
+
+    public static final String get =
+            """
+                    SELECT * FROM public.questions
+                    WHERE id = ?
+                    """;
+
+    public static final String getAll =
+            """
+                    SELECT * FROM public.questions
+                    """;
+    public static final String remove =
+            """
+                    DELETE FROM public.questions
+                    WHERE id = ?
+                    """;
+
+    public static final String update =
+            """
+                    UPDATE public.questions
+                    SET name = ?,
+                    WHERE id = ?;
+                    """;
+
+
+    public QuestionRepositoryPostgres(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public boolean saveQuestion(Question question) {
+        try {
+            var preparedStatement = connection.prepareStatement(save);
+
+            preparedStatement.setString(1, question.getText());
+            return preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Question getQuestion(int id) {
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(get);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery(get + id);
+            resultSet.next();
+
+            return Question.builder()
+                    .id(resultSet.getInt("id"))
+                    .text(resultSet.getString("text"))
+                    .build();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Question> getAllQuestions() {
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getAll);
+            List<Question> questions = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Question build = Question.builder()
+                        .text(resultSet.getString(2))
+                        .id(resultSet.getInt(1))
+                        .topic_id(resultSet.getInt(3))
+                        .build();
+
+                questions.add(build);
+            }
+            return questions;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int updateQuestion(Question question) {
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(update);
+            preparedStatement.setInt(1, question.getId());
+            preparedStatement.setString(2, question.getText());
+
+            return preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean removeQuestion(int id) {
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(remove);
+
+            preparedStatement.setInt(1, id);
+            return preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/hillel/zakushniak/repository/TopicRepositoryPostgres.java
@@ -0,0 +1,132 @@
+package com.hillel.zakushniak.repository;
+
+import com.hillel.zakushniak.model.Topic;
+import com.hillel.zakushniak.repository.dao.TopicRepository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TopicRepositoryPostgres implements TopicRepository {
+
+    private final Connection connection;
+
+    public static final String save =
+            """
+                    INSERT INTO public.topics (name)
+                    VALUES (?)
+                    """;
+
+    public static final String get =
+            """
+                   SELECT * FROM public.topics
+                    WHERE id = ?
+                    """;
+
+    public static final String getAll =
+            """
+                    SELECT * FROM public.topics
+                    """;
+
+
+    public static final String remove =
+            """
+                    DELETE FROM public.topics
+                    WHERE id = ?
+                    """;
+
+    public static final String update =
+            """
+                    UPDATE public.topics
+                    SET name = ?,
+                    WHERE id = ?;
+                    """;
+
+
+    public TopicRepositoryPostgres(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public boolean saveTopic(Topic topic) {
+        try {
+            var preparedStatement = connection.prepareStatement(save);
+
+            preparedStatement.setString(1, topic.getName());
+            return preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Topic getTopic(int id) {
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(get);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery(get + id);
+            resultSet.next();
+
+            return Topic.builder()
+                    .id(resultSet.getInt("id"))
+                    .name(resultSet.getString("name"))
+                    .build();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Topic> getAllTopics() {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getAll);
+            List<Topic> topics = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Topic build = Topic.builder()
+                        .name(resultSet.getString(2))
+                        .id(resultSet.getInt(1))
+                        .build();
+
+                topics.add(build);
+            }
+            return topics;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int updateTopic(Topic topic) {
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(update);
+            preparedStatement.setInt(1, topic.getId());
+            preparedStatement.setString(2, topic.getName());
+
+            return preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean removeTopic(int id) {
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(remove);
+
+            preparedStatement.setInt(1, id);
+            return preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
